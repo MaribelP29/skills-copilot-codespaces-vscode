d5abeb7dff26ec081886f9fcc9a4219d928a93a6
Maribel Pereyra Carballal <75271834+MaribelP29@users.noreply.github.com>
2025-02-24 17:34:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillsMember() {
+  const skills = [
+    'JavaScript',
+    'React',
+    'React Native',
+    'Node.js',
+    'MongoDB',
+    'Firebase',
+    'Docker',
+    'Kubernetes',
+    'CI/CD',
+    'DevOps',
+  ];
+  return skills;
+}
